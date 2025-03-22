7b0fe4be071d150c3dfac29222f968a7d7d08605
Aimerence <liusijia1942@gmail.com>
2025-03-22 23:21:20 +0800

    Merge updates from upstream

--- src/config.ts
+++ src/config.ts
@@ -17,4 +17,6 @@ export const SITE = {
     url: "https://github.com/Aimerence/aimer-blog/edit/main",
   },
   dynamicOgImage: true,
+  lang: "zh-CN", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
